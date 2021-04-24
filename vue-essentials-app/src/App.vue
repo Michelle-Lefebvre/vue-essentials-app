3632f9ed469450b5a783f11f80803735c498b086
Michelle Lefebvre <mmldesigns007@gmail.com>
2021-04-23 23:10:21 -0400

    update price

--- vue-essentials-app/src/App.vue
+++ vue-essentials-app/src/App.vue
@@ -1,19 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mt-5">
+    <h1>My Shop</h1>
+    <price-slider :sliderStatus="sliderStatus"></price-slider>
+    <product-list
+      :maximum="maximum"
+      :products="products"
+      @add="addItem"
+    ></product-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import ProductList from "./components/ProductList.vue";
+import PriceSlider from "./components/PriceSlider.vue";
 
 export default {
-  name: 'App',
+  name: "app",
+  data: function () {
+    return {
+      maximum: 99,
+      cart: [],
+      sliderStatus: true,
+      products: null,
+    };
+  },
+  methods: {
+    addItem: function (product) {
+      var whichProduct;
+      var existing = this.cart.filter(function (item, index) {
+        if (item.product.id == Number(product.id)) {
+          whichProduct = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+      if (existing.length) {
+        this.cart[whichProduct].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    // FontAwesomeIcon,
+    ProductList,
+    PriceSlider,
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+};
 </script>
 
 <style>
---
+++ vue-essentials-app/src/components/PriceSlider.vue
@@ -0,0 +1,48 @@
+<template>
+  <transition name="fade">
+    <div v-if="sliderStatus">
+      <div class="align-items-center" :class="sliderStatus">
+        <label :class="labelArr" for="formMax">max</label>
+        <input
+          type="text"
+          id="formMax"
+          class="form-control mx-2"
+          :style="{ width: inputWidth + 'px', textAlign: 'center' }"
+          v-model="maximum"
+        />
+        <input
+          type="range"
+          class="custom-range"
+          min="0"
+          max="200"
+          v-model="maximum"
+        />
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+
+export default {
+    name: 'PriceSlider',
+    props: ['sliderStatus'],
+    computed: {
+        sliderStatus: function () {
+            return this.sliderStatus ? "d-flex" : "d-none";
+        }
+    },
+}
+</script>
+
+<style scoped>
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s ease-in-out;
+}
+</style>
